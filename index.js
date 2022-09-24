c07e0391cc44d201d68b57f941a3b3cb5b9d7a37
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2022-09-24 18:58:45 -0500

    Upgrade bare server and type module

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-(async() => {
-  await import('./index.mjs');
-})();
+import createBareServer from '@tomphttp/bare-server-node';
+import http from 'http';
+
+const PORT = process.env.PORT || 8080
+
+/*
+{
+	name: 'TOMPHTTP NodeJS Bare Server Deployable',
+	repostory: 'https://github.com/IDontCodee/bare-server-deployable'
+}
+*/
+
+const bare = createBareServer('/')
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    if (bare.shouldRoute(req)) {
+		bare.routeRequest(req, res);
+	} else {
+        res.writeHead(400);
+		res.send('400 Bad Request');
+	}
+});
+
+server.on('upgrade', (req, socket, head) => {
+	if (bare.shouldRoute(req, socket, head)) {
+		bare.routeUpgrade(req, socket, head);
+	} else {
+		socket.end();
+	}
+});
+
+server.on('listening', () => {
+	console.log(`Bare server started on port ${PORT}`)
+});
+
+server.listen({ port: PORT });
